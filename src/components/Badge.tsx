006b148d2a223210c116f288cfc9d656d46df0c4
NataliyaChe <happylibrarygeek@gmail.com>
2023-09-28 00:16:38 +0300

    badge fixed

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -4,11 +4,16 @@ import * as ItemActionCreators from '../store/actions/itemAction'
 
 function Badge () {
     const {shoppingCart} = useTypedSelector(state => state.item)
+    const cartLength = shoppingCart.length
+    console.log('cartLength', cartLength);
+    
 
     return (
-        <div className="badge">
+        // {cartLength > 0 && 
+            <div className="badge">
             <span className="badge-text">{shoppingCart.length}</span>
         </div>
+        // }
     )
 }
 
