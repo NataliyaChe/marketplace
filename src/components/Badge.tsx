98614a962eb50b8a0c2df88e41236a9a20f00cfc
NataliyaChe <happylibrarygeek@gmail.com>
2023-10-09 14:44:51 +0300

    badge fixed

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,19 +1,12 @@
-import { useActions } from "../hooks/useActions"
 import { useTypedSelector } from "../hooks/useTypedSelector"
-import * as ItemActionCreators from '../store/actions/itemAction'
 
 function Badge () {
     const {shoppingCart} = useTypedSelector(state => state.item)
-    const cartLength = shoppingCart.length
-    console.log('cartLength', cartLength);
     
-
     return (
-        // {cartLength > 0 && 
-            <div className="badge">
+        <div className="badge">
             <span className="badge-text">{shoppingCart.length}</span>
         </div>
-        // }
     )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,13 +7,17 @@ import Badge from './Badge';
 
 function Header () {
     const {setModal} = useActions(ItemActionCreators)
+    const {shoppingCart} = useTypedSelector(state => state.item)
+    const cartLength = shoppingCart.length
 
     return (
         <div className="header">
             <Link to='.' className='title'>
                 Marketplace
             </Link>
-            <Badge />
+            {cartLength > 0 && 
+                <Badge />
+            }       
             <button className="button cart-button" onClick={setModal}>Cart</button>
         </div>
     )
